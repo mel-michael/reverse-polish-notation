1bd4e03f937fd3234149fcb0981d8efb610bbc0a
Mel Michael <mel.michael007@gmail.com>
2019-09-18 11:08:30 +0100

    Code clean up

--- index.js
+++ index.js
@@ -3,24 +3,25 @@
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 
-console.log(chalk.green('\n ✓ Running Reverse Polish Notation Calculator \n\n')); 
+console.log(chalk.green('\n ✓ Running Reverse Polish Notation Calculator \n'));
 
-function calculateRPN(expression) {
+function calculateRPN(expr) {
 	const stack = [];
-	const operators = [ "*", "/", "-", "+"]; // restrict to these 4
+	const operators = ["*", "/", "-", "+"]; // restrict to these 4
+	const expression = expr.split(' ');
 
-	for(let index = 0; index < expression.length; index++) {
+	for (let index = 0; index < expression.length; index++) {
 		const token = expression[index];
-		if(Number(token)) {
+		if (Number(token)) {
 			stack.push(token);
 			continue;
 		}
-		if(operators.indexOf(token) > -1) {
+		if (operators.indexOf(token) > -1) {
 			const operator = operators[operators.indexOf(token)];
 			const exp2 = stack.pop();
 			const exp1 = stack.pop();
 			const result = exp1 ? eval(`${exp1}${operator}${exp2}`) : exp2;
-			stack.push(result)
+			stack.push(result);
 		}
 	}
 	return stack.pop();
@@ -28,14 +29,14 @@ function calculateRPN(expression) {
 
 
 inquirer
-  .prompt([
+	.prompt([
 		{
 			type: 'input',
 			name: 'expression',
-			message: "Please enter a mathematical expression: "
+			message: "Please enter a reverse Polish notation:"
 		}
-  ])
-  .then(response => {
-		const calcRPN = calculateRPN(response.expression);
-		console.log(chalk.blue(`\n The result of the expression is:`), chalk.green(`${calcRPN} \n`))
+	])
+	.then(response => {
+		const solution = calculateRPN(response.expression);
+		console.log(chalk.blue(`\n The result of the expression is:`), chalk.green(`${solution} \n`))
 	});
